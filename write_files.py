6b44acf31d0541b5db4e6c56e670a3f86bff90ea
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-21 12:31:46 +0700

    Program for inputting the story

--- write_files.py
+++ write_files.py
@@ -1,3 +1,12 @@
 # Define a function called write_to_file:
-# Print a message to start writing the life story.
-# Ask the user if there are more lines to add.
+def write_to_file():
+        life_story = [] 
+        print("Let's write your life story!\n")  # Print a message to start writing the life story.
+        while True:
+            line = input("Enter line: ")
+            life_story.append(line)
+            more_lines = input("\nAre there more lines? (yes/no): ") # Ask the user if there are more lines to add.
+            if more_lines.lower() != 'yes':
+                break 
+
+write_to_file()
