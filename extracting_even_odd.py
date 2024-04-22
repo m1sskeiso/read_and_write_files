ee5ee81232e21fd2ed3eaad0724c9189610aba6d
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-22 09:13:12 +0700

    Pseudocode

---
+++ extracting_even_odd.py
@@ -0,0 +1,6 @@
+# Define the input file name
+# Initialize lists to store even numbers and odd numbers
+# Open the input file in read mode
+# Append even numbers to even_numbers list and odd numbers to odd_numbers list
+# Write even numbers to 'even.txt'
+# Write odd numbers to 'odd.txt'
