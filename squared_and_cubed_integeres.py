115c9ae73a37b65e54459cd9b7e10f15dc262e8b
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-22 07:11:51 +0700

    odd and even

--- squared_and_cubed_integeres.py
+++ squared_and_cubed_integeres.py
@@ -5,6 +5,10 @@ input_file = 'integers.txt'
 with open(input_file, 'r') as file:
     # Read all lines from the file and convert them to integers
     numbers = [int(num) for num in file.readlines()]
-    
+
 # Create a list containing the square of each even number from the input file
+    even_squares = [str(num**2) for num in numbers if num % 2 == 0]
+
 # Create a list containing the cube of each odd number from the input file
+    odd_cubes = [str(num**3) for num in numbers if num % 2 != 0]
+
