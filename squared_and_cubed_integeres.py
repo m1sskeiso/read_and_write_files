27af00496a10c3a1c3d7631aaf5f549e591bac56
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-22 07:08:32 +0700

    put the file in read mode

--- squared_and_cubed_integeres.py
+++ squared_and_cubed_integeres.py
@@ -1,4 +1,10 @@
 # Define the input file name
+input_file = 'integers.txt'
+
 # Open the input file in read mode
+with open(input_file, 'r') as file:
+    # Read all lines from the file and convert them to integers
+    numbers = [int(num) for num in file.readlines()]
+    
 # Create a list containing the square of each even number from the input file
 # Create a list containing the cube of each odd number from the input file
