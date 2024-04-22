d4d08bc674331b365180b2879f964916f1303da7
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-22 09:11:31 +0700

    Change file error

--- extracing_odd_and_even.py
+++
@@ -1,7 +0,0 @@
-# Define the input file name
-input_file = 'numbers.txt'
-
-# Initialize lists to store even numbers and odd numbers
-# Open the input file in read mode
-# Write even numbers to 'even.txt'
-# Write odd numbers to 'odd.txt'
