a2dc88fab268b350807ab74142bf3c3c38336423
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-04-22 07:05:40 +0700

    Pseudocode and integers

--- gwa_students_reader.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('students_gwa.csv', 'r') as file:
-    reader = csv.reader(file)
-
-    for line in reader:
-        print(line)
---
+++ squared_and_cubed_integeres.py
@@ -0,0 +1,4 @@
+# Define the input file name
+# Open the input file in read mode
+# Create a list containing the square of each even number from the input file
+# Create a list containing the cube of each odd number from the input file
